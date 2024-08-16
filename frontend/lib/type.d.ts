b85cb79f4a62516580283e1fa308f13fa0b68764
ysh7011 <tpghksmega@gmail.com>
2024-08-16 11:18:35 +0900

    feat: 타입관리 전역에서함

--- frontend/lib/type.d.ts
+++ frontend/lib/type.d.ts
@@ -3,3 +3,23 @@ declare module "react-syntax-highlighter/*"
 declare module "uuid"
 declare module "tailwindcss"
 declare module "dompurify"
+
+type FileType = "FOLDER" | "FILE"
+
+interface FileData {
+  basename: string;
+  file_id: string;
+  file_type: FileType;
+  path: string;
+  children: FileData[];
+}
+
+type RootFile = "core_memory_files" | "recall_memory_files";
+
+interface FirebaseUser {
+  isLoading?: boolean;
+  isSignedIn?: boolean;
+  currentUser?: User | null;
+  signInWithGoogle?: () => Promise<UserCredential>;
+  signOut?: () => Promise<void>;
+}
