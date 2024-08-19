935fc24e21cd29106f9705775b593c977b50bfe0
ysh7011 <tpghksmega@gmail.com>
2024-08-20 08:52:53 +0900

    docs: lib/firebase.config.ts 변수 초기화

---
+++ frontend/lib/firebase.config.ts
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Firebase configuration object with environment variables
+const firebaseConfig = {
+  //..
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage };
