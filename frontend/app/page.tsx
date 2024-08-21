512a3f92a58eeb9279622fc68ee88ee26ff15ab5
ysh7011 <tpghksmega@gmail.com>
2024-08-21 13:45:46 +0900

    docs: 첫 페이지에만 context 관리

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,26 +5,30 @@ import Sidebar from "@/components/common/sidebar";
 import { useThreadNavigation } from "@/hooks/use-thread-navigation";
 import { FirebaseUserContext } from "@/lib/firebase-user";
 import { FirestoreMessageData } from "@/types/message";
-import React, { useContext, useState } from "react";
+import React, { useContext, useReducer, useState } from "react";
+import { memoryContext, initialState, memoryReducer } from "./memory-reducer";
 
 const MainPage: React.FC = () => {
   const [messages, setMessages] = useState<FirestoreMessageData[]>([]);
   const user = useContext(FirebaseUserContext);
   const uid = user.currentUser?.uid;
-  const sendMessage = useThreadNavigation(uid);
+  const [memoryState, memoryDispatch] = useReducer(memoryReducer, initialState);
+  const sendMessage = useThreadNavigation(uid, memoryState);
 
   const noOpDelete = async (messageId: string) => {
     return;
   };
 
   return (
-    <Sidebar>
-      <ChatContainer
-        messages={messages}
-        onMessageSubmit={sendMessage}
-        onMessageDelete={noOpDelete}
-      />
-    </Sidebar>
+    <memoryContext.Provider value={[memoryState, memoryDispatch]}>
+      <Sidebar>
+        <ChatContainer
+          messages={messages}
+          onMessageSubmit={sendMessage}
+          onMessageDelete={noOpDelete}
+        />
+      </Sidebar>
+    </memoryContext.Provider>
   );
 };
 
