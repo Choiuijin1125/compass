be0954355313285e0fba3edfed0f28264fd3cdd6
ysh7011 <tpghksmega@gmail.com>
2024-08-21 13:47:26 +0900

    docs: 첫 페이지에서 send 버튼을 눌러서 대화를 전송할 때 context도 같이 전달

--- frontend/hooks/use-thread-navigation.ts
+++ frontend/hooks/use-thread-navigation.ts
@@ -2,13 +2,13 @@
 import { createThread } from "@/lib/firebase/threads";
 import { useRouter } from "next/navigation";
 
-export const useThreadNavigation = (uid: string | undefined) => {
+export const useThreadNavigation = (uid: string | undefined, memoryState: any) => {
   const router = useRouter();
 
   const sendMessage = async (userMsg: string) => {
     if (!uid) return;
 
-    const threadId = await createThread(uid, userMsg);
+    const threadId = await createThread(uid, userMsg, memoryState);
     router.push(`/threads/${threadId}`);
   };
 
